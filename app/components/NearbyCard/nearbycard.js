335ba6c07c72a4f8fac84f8b61f7d795e8f49165
Sleightttt <brennancota@yahoo.com>
2023-08-07 13:45:19 -0700

    added nearby card

--- app/components/NearbyCard/nearbycard.js
+++ app/components/NearbyCard/nearbycard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
-
+import { checkImageURL } from "../../utils";
 import styles from "./nearbycard.style";
 
 const NearbyCard = ({ item, selectedJob, handleCardPress }) => {
@@ -11,7 +11,11 @@ const NearbyCard = ({ item, selectedJob, handleCardPress }) => {
     >
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image
-          source={{ uri: item.employer_logo }}
+          source={{
+            uri: checkImageURL(item.employer_logo)
+              ? item.employer_logo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
           resizeMode="contain"
           style={styles.logoImage}
         />
@@ -23,6 +27,7 @@ const NearbyCard = ({ item, selectedJob, handleCardPress }) => {
         <Text numberOfLines={2} style={styles.jobName(selectedJob, item)}>
           {item.job_title}
         </Text>
+        <Text style={styles.location}>{item.job_country}</Text>
       </View>
     </TouchableOpacity>
   );
